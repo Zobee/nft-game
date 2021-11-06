790bdeafdfbfc2d4938f6554905a5aa6767e2e81
Zoheb Boga <zohebboga@hotmail.com>
2021-11-05 21:26:32 -0400

    Initialize contract to generate characters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,36 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol"; 
 
 contract MyEpicGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
+  struct CharAttrs {
+    uint256 charInd;
+    string name;
+    string imgURI;
+    uint256 hp;
+    uint256 maxHp;
+    uint256 attack;
+  }
+
+  CharAttrs[] defaultChars;
+
+  constructor(
+    string[] memory charNames,
+    string[] memory charImgURIs,
+    uint256[] memory charHp,
+    uint256[] memory charAttack
+  ) {
+
+    for(uint i = 0; i < charNames.length; i += 1){
+      defaultChars.push(CharAttrs({
+        charInd: i,
+        name: charNames[i],
+        imgURI: charImgURIs[i],
+        hp: charHp[i],
+        maxHp: charHp[i],
+        attack: charAttack[i]
+      }));
+
+      CharAttrs memory c = defaultChars[i];
+      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imgURI);
+    }
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,14 @@ const main = async () => {
   //Compile our contract
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   //Deploy our contract
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Jack Black", "Jack White", "Samurai Jack"],
+    ["https://imgur.com/gallery/ylOYBpT",
+    "https://imgur.com/gallery/1r3XKWC",
+    "https://imgur.com/t/samurai_jack/uSZ9ckg"],
+    [350, 200, 100],
+    [100, 150, 225]
+  );
   //wait for the resolution of the deploy method
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
