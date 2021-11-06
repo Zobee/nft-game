97896c30f6591b20cbafa6edc3fb2c373b7bfd2f
Zoheb Boga <zohebboga@hotmail.com>
2021-11-05 22:28:37 -0400

    Override tokenURI method to print base64 data

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -11,6 +11,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+// Base64 Encoding
+import "./libraries/Base64.sol";
+
 contract MyEpicGame is ERC721 { //ERC721 is the standard NFT contract. Our contract will inherit from it
   struct CharAttrs {
     uint256 charInd;
@@ -54,24 +57,53 @@ contract MyEpicGame is ERC721 { //ERC721 is the standard NFT contract. Our contr
       _tokenIDs.increment();
     }
 
-    function mintJackNFT(uint _charInd) external {
-      // Get current tokenId (starts at 1 since we incremented in the constructor).
-      uint newItemId = _tokenIDs.current();
-      _safeMint(msg.sender, newItemId);
+  function mintJackNFT(uint _charInd) external {
+    // Get current tokenId (starts at 1 since we incremented in the constructor).
+    uint newItemId = _tokenIDs.current();
+    _safeMint(msg.sender, newItemId);
 
-      nftHolderAttrs[newItemId] = CharAttrs({
-        charInd: _charInd,
-        name: defaultChars[_charInd].name,
-        imgURI: defaultChars[_charInd].imgURI,
-        hp: defaultChars[_charInd].hp,
-        maxHp: defaultChars[_charInd].hp,
-        attack: defaultChars[_charInd].attack
-      });
+    nftHolderAttrs[newItemId] = CharAttrs({
+      charInd: _charInd,
+      name: defaultChars[_charInd].name,
+      imgURI: defaultChars[_charInd].imgURI,
+      hp: defaultChars[_charInd].hp,
+      maxHp: defaultChars[_charInd].hp,
+      attack: defaultChars[_charInd].attack
+    });
 
-      console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _charInd);
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _charInd);
 
-      nftHolders[msg.sender] = newItemId; 
+    nftHolders[msg.sender] = newItemId; 
 
-      _tokenIDs.increment();
+    _tokenIDs.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory){
+    CharAttrs memory charAttrs = nftHolderAttrs[_tokenId];
+
+    string memory strHp = Strings.toString(charAttrs.hp);
+    string memory strMaxHp = Strings.toString(charAttrs.maxHp);
+    string memory strAttack = Strings.toString(charAttrs.attack);
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "}',
+            charAttrs.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            "image: ",
+            charAttrs.imgURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',strAttack,'} ]}'
+          )
+        )
+      )
+    );
+    string memory output = string(
+    abi.encodePacked("data:application/json;base64,", json)
+    );
+  
+      return output;
   }
 }
